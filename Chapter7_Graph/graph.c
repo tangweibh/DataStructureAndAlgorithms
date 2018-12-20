23c934c8ce238a4e78f0262df23761a4f7f8577f
Wei Tang <tangweibh@gmail.com>
2018-12-20 13:57:01 -0600

    WFS

--- Chapter7_Graph/graph.c
+++ Chapter7_Graph/graph.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct QueueNode{
+  int data;
+  struct QueueNode *next;
+} QueueNode, *Queue;
+
+void EnQueue(Queue Q, int e) {
+  QueueNode *p_node = (QueueNode*)malloc(sizeof(QueueNode));
+  p_node->data = e;
+  p_node->next = NULL;
+  QueueNode *p_rear = Q;
+  while (p_rear->next) {
+    p_rear = p_rear->next;
+  }
+  p_rear->next = p_node;
+}
+
+int DeQueue(Queue Q, int *e) {
+  if (!Q->next) return 0;
+  QueueNode *p_node = Q->next;
+  *e = p_node->data;
+  Q->next = p_node->next;
+  free(p_node);
+  return 1;
+}
+
 typedef struct Graph {
   char *vex;
   int *adj;
@@ -41,6 +66,31 @@ void DFSTraverse(Graph G) {
   DFS(G, visited, 0);
 }
 
+void WFS(Graph G, int *visited, Queue Q) {
+  int e, i;
+  if (!DeQueue(Q, &e) || visited[e]) return;
+  visited[e] = 1;
+  printf("%c ", G.vex[e]);
+  for (i = 0; i < G.vex_num; i++) {
+    if (!visited[i] && G.adj[e*G.vex_num + i])
+      EnQueue(Q, i);
+  }
+  while (Q->next) {
+    WFS(G, visited, Q);
+  }
+}
+
+void WFSTraverse(Graph G) {
+  Queue Q = (Queue)malloc(sizeof(QueueNode));
+  EnQueue(Q, 0);
+  int *visited = (int*)malloc(G.vex_num * sizeof(int));
+  int i;
+  for (i = 0; i < G.vex_num; i++) {
+    visited[i] = 0;
+  }
+  WFS(G, visited, Q);
+}
+
 int main () {
   int adj_mat[] = {
     0, 1, 0, 0, 0, 1, 0, 0, 0,
@@ -64,5 +114,19 @@ int main () {
   //}
   // DFS traverse
   DFSTraverse(*p_G);
+  printf("\n");
+
+  // Queue
+  //Queue Q = (Queue)malloc(sizeof(QueueNode));
+  //for (i = 0; i < 5; i++) {
+  //  EnQueue(Q, i*i);
+  //}
+  //while (1) {
+  //  j = DeQueue(Q, &i);
+  //  if (j) printf("%d ", i);
+  //  else break;
+  //}
+  // WFS traverse
+  WFSTraverse(*p_G);
   return 0;
 }
